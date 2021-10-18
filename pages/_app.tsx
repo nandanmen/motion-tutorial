2b4e365743a624850805c4981e155cfce237f4fc
Nanda Syahrasyad <nanda.s@hey.com>
2021-10-17 20:53:50 -0700

    feat: initial boilerplate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { globalCss } from "@/stitches";
+
+const globalStyles = globalCss({
+  "*": {
+    margin: 0,
+    padding: 0,
+    "-webkit-font-smoothing": "antialiased",
+    "-moz-osx-font-smoothing": "grayscale",
+    "text-rendering": "optimizeLegibility",
+  },
+  html: {
+    background: "$mint1",
+    color: "$mint12",
+    fontFamily: "$sans",
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  globalStyles();
+  return <Component {...pageProps} />;
 }
-export default MyApp
+export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { getCssText } from "@/stitches";
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <style
+            id="stitches"
+            dangerouslySetInnerHTML={{ __html: getCssText() }}
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
